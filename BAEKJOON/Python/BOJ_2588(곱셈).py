27812a29479ac0a6b1e275143d815c8db88aa3ac
Yetaeng <56065130+Yetaeng@users.noreply.github.com>
2020-02-14 02:49:40 +0900

    Delete BOJ_2588(곱셈).py

--- BAEKJOON/Python/BOJ_2588(곱셈).py
+++
@@ -1,8 +0,0 @@
-A = int(input())
-B = int(input())
-temp = B
-for i in range(3): # i를 3번 반복해라(종료값이 3이면 0,1,2로 3번 반복)
-  print(i)
-  print(A * (temp % 10)) # temp 값을 10으로 나눈 나머지 값은 입력 값 B의 일의자리를 의미
-  temp = temp // 10 # temp 값을 10으로 나눈 몫을 temp에 새롭게 넣음
-print(A*B)
