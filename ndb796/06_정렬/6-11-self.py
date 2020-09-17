6999b98e789f6b922b167a9bf80b2f13b0fe7e49
Yetaeng <56065130+Yetaeng@users.noreply.github.com>
2020-09-17 10:26:50 +0900

    [정렬] 성적이 낮은 순서로 학생 출력하기 입력 받는 부분 수정

--- ndb796/06_정렬/6-11-self.py
+++ ndb796/06_정렬/6-11-self.py
@@ -3,12 +3,13 @@ n = int(input())
 arr = []
 for i in range(n):
     input_data = input().split()
-    arr.append(input_data[0], int(input_data[1]))
+    # input_data[0], int(input_data[1]를 괄호로 묶어서 한개로 처리해줘야함! 안그러면 append에는 인자가 하나들어가야하므로 2개가 들어갔다는 에러가 뜸
+    arr.append((input_data[0], int(input_data[1])))
 
 arr = sorted(arr, key=lambda student: student[1])  # 정렬 조건이니까! 정렬시킬때 람다식으로 주자!
+# arr을 정렬할껀데, student를 매개변수로 설정해서 student의 두번째 요소를 기준으로 정렬하는 키를 만들꺼야
 
 for student in arr:
-    print(student[0], end='')
-
+    print(student[0], end=' ')
 
 # lambda 식에 student는... 보충 공부 필요!
