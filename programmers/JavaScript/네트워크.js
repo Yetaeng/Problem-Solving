d36ca97c975674175fe34eee2796fdade75eac29
yetaeng <yetaeng20@gmail.com>
2022-11-04 00:20:00 +0900

    [DFS/BFS] 네트워크

---
+++ programmers/JavaScript/네트워크.js
@@ -0,0 +1,22 @@
+function solution(n, computers) {
+  let answer = 0;
+  let visited = [];
+  
+  function dfs(idx) {
+      visited[idx] = true;
+      for (let i=0; i<computers[idx].length; i++) {
+          if (computers[idx][i] === 1 && !visited[i]) {
+              dfs(i);
+          }
+      }
+  }
+  
+  for (let i=0; i<n; i++) {
+      if (!visited[i]) {
+          dfs(i)
+          answer++;
+      }
+  }
+  
+return answer;
+}
